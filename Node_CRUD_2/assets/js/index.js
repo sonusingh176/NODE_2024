a670d6ad257d3d0545a64c8b45bc089a82492b01
sonusingh <sonusingh82601@gmail.com>
2024-01-18 00:32:35 +0530

    basic node crud

--- Node_CRUD_2/assets/js/index.js
+++ Node_CRUD_2/assets/js/index.js
@@ -4,4 +4,28 @@ $('#add_user').submit((event)=>{
 
 $('#update_user').submit(function(event){
     event.preventDefault();
+    var unindexed_array =$(this).serializeArray();
+    console.log(unindexed_array);
+
+    var data={};
+    $.map(unindexed_array,function(n,i){
+        data[n['name']]=n['value']
+    })
+    console.log(data);  
+
+    var request={
+        url:`http://localhost:3000/api/users/${data.id}`,
+        method :"put",
+        data : data,
+        contentType:'application/json',
+        CORS: true ,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*'
+            },
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Information updated successfully");
+    })
 })
